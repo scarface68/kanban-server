f7f8df0e0def12c75285b1835a7e50ef4719f986
Sai Chandan Mohanty <saichandan68@gmail.com>
2023-11-10 10:50:25 +0530

    fixing utils and cleaning gitignore

--- utils/auth-helper.js
+++ utils/auth-helper.js
@@ -1,6 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateAuthToken = (user) => {
+  /**
+   * Generates a JSON Web Token (JWT) for the given user.
+   *
+   * @param {Object} user - The user object to generate the token for.
+   * @param {string} user.email - The email address of the user.
+   * @param {string} user._id - The unique ID of the user.
+   * @returns {string} The generated JWT.
+   */
   const token = jwt.sign({ email: user.email, userId: user._id }, "secret", {
     expiresIn: "1h",
   });
