7e5a1a9c79f8caa785e513366c5217c699ce3839
Sai Chandan Mohanty <saichandan68@gmail.com>
2023-11-10 13:07:51 +0530

    cors fix

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const cors = require("cors"); // Import cors package
 const app = express();
 
-app.use(cors({
-  origin: ['https://kanban-kraftbase.vercel.app/', 'http://localhost:3000']
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 const boardRoutes = require("./routes/board");
 const listRoutes = require("./routes/list");
@@ -29,14 +32,12 @@ app.use("/card", cardRoutes);
 
 app.use(sendErrorResponse);
 
-app.get('/', (req, res) => {
-  res.send('welcome to my api');
-})
+app.get("/", (req, res) => {
+  res.send("welcome to my api");
+});
 
 mongoose
-  .connect(
-    `${process.env.MONGODB_URI}`,
-  )
+  .connect(`${process.env.MONGODB_URI}`)
   .then(() =>
     app.listen(process.env.PORT || 5000, () => {
       console.log("Server started on port 5000");
